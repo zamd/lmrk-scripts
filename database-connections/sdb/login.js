077643dc83dbf25f9af0efa63705b40a4faa4384
Zulfiqar Ahmed <zamd@users.noreply.github.com>
2016-09-14 18:13:28 +0100

    auth0->auth0 login

---
+++ database-connections/sdb/login.js
@@ -0,0 +1,21 @@
+function login (email, password, callback) {
+
+  var data = {
+    username: email,
+    password: password,
+    connection: configuration.connection
+  };
+  var AuthenticationClient = require('auth0@2.0.0').AuthenticationClient;
+  var auth0 = new AuthenticationClient({
+    domain: configuration.domain,
+    clientId: configuration.clientID
+  });
+  
+  auth0.database.signIn(data, function(err,res){
+    if (err)
+      return callback(new Error(err));
+    auth0.getProfile(res.access_token, function(err, profile){
+      return callback(err, profile);
+    });
+  });
+}
